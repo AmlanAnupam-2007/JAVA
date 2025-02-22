3bbe19551804c837e2cd702356b14d908b47ed21
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2025-02-22 18:50:48 +0530

    hi

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hi");
+        System.out.print("\nAmlan ");
+        int Var1 = 2;
+        int Var2 = 3;
+        int Var3 = Var1+Var2;
+        System.out.println(Var3+" This is the answer.");
+        
+        
+    }
+
+}
